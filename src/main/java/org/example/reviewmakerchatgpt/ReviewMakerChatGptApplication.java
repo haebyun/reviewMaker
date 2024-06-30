d1d6ce3fba482700d52723e615357fea9314c635
haebyun <bhk4392@gmail.com>
2024-06-30 13:59:38 +0900

    init

---
+++ src/main/java/org/example/reviewmakerchatgpt/ReviewMakerChatGptApplication.java
@@ -0,0 +1,13 @@
+package org.example.reviewmakerchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMakerChatGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewMakerChatGptApplication.class, args);
+    }
+
+}
